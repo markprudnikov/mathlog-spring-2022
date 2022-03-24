98d502a5a4ba76dea5d2c8fe4527c42d5dd9607e
Mark Prudnikov <mark.prudnikov@inbox.ru>
2022-03-24 13:31:12 +0300

    Pretty printer

--- n-queens-problem/app/Main.hs
+++ n-queens-problem/app/Main.hs
@@ -4,15 +4,26 @@ import QueenFormulaGen ( getCNF, fromSingleToPair )
 import Picosat ( solve, Solution(Solution) )
 import System.Environment (getArgs)
 
-solutionToString :: Int -> Solution -> String
-solutionToString _ (Solution []) = ""
-solutionToString n (Solution (x:xs)) | x < 0 = solutionToString n (Solution xs)
-                                     | otherwise = let p = fromSingleToPair n x in "\n" ++ show (fst p + 1) ++ " " ++ show (snd p + 1) ++ solutionToString n (Solution xs)
-solutionToString _ _ = "\nProblem has no solution"
+replace :: Int -> a -> [a] -> [a]
+replace pos newVal list = take pos list ++ newVal : drop (pos+1) list
+
+setQueen :: (Int, Int) -> [[Char]] -> [[Char]]
+setQueen (x,y) pole = replace x (replace y 'Q' (pole !! x)) pole
+
+setAllQueens :: Int -> Solution -> [[Char]] -> [[Char]]
+setAllQueens n (Solution []) pole = pole
+setAllQueens n (Solution (x:xs)) pole | x < 0 = setAllQueens n (Solution xs) pole
+                                      | otherwise = let p = fromSingleToPair n x in setAllQueens n (Solution xs) $ setQueen p pole
+setAllQueens n _ _ = ["Problem has no solution"]
+
+setToString :: [[Char]] -> String
+setToString ["Problem has no solution"] = "Problem has no solution"
+setToString lst = "----------\nSolution:\n----------\n" ++ foldr (\slovo -> (("[" ++ slovo ++ "]\n") ++)) [] lst ++ "----------"
 
 main :: IO ()
 main = do
     input <- getArgs
     let n = read $ head input :: Int
+    let pole = ([ replicate n x | x <- replicate n '.'])
     solution <- solve (getCNF n)
-    putStrLn ("----------\nSolution:\n----------" ++ solutionToString n solution ++ "\n----------")
+    putStrLn (setToString (setAllQueens n solution pole))
